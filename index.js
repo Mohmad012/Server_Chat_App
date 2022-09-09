cd73800b8d78e214897b14d958a45385929720d7
Mohmad012 <mohmadailsia@gmail.com>
2022-09-09 23:41:30 +0200

    finished code of back

---
+++ index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+// word for on must be as a lower case
+io.on("connection", (socket) => {
+  console.log("connection...");
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log("Your is join room: " + room);
+  });
+
+  socket.on("enter_room", (data) => {
+    console.log(data.text, " Has Enter The Room...");
+    socket.to(data.room).emit("Who_enter_room", data.text);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log("Recived FullData: ", data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("leave_room", (data) => {
+    console.log(data.text, " Has Left...");
+    socket.to(data.room).emit("Who_left_room", data.text);
+  });
+
+  // word for on must be as a lower case
+  socket.on("disconnect", () => {
+    console.log("DisConnect...");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is Running on 3001");
+});
